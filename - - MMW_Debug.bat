53eb3dea9c4a3bc71cb4d4cc1aed7753af4f86ad
Durgan <72322229+DRGN-DRC@users.noreply.github.com>
2021-08-18 14:58:45 -0700

    Adding dependencies

--- - - MMW_Debug.bat
+++ - - MMW_Debug.bat
@@ -1,8 +1,10 @@
 @echo off
 
-python "%~dp0main.py" %*
+
+C:\Python27\python.exe "%~dp0main.py" %*
 
 
+:: Exit if no problems, otherwise pause so we can see what happened.
 if [%ERRORLEVEL%]==[0] goto eof
-
-pause
+pause > nul
+echo Press any key to exit . . .
---
+++ Dependencies/Install Dependencies.bat
@@ -0,0 +1,90 @@
+:: Created by DRGN
+:: 8/18/2021
+:: v1.0
+@echo off
+setlocal EnableDelayedExpansion
+
+
+	:: Set an install directory, and 
+	:: see if Python is installed there.
+
+set pythonDir=C:\Python27
+if exist "%pythonDir%\python.exe" (
+    echo Skipping Python installation; already installed.
+    goto ModuleInstallation
+)
+
+
+	:InstallPython
+
+:: Get the python installer filename and python version
+for /R .\ %%G in (python-*.msi) do set pythonInstaller=%%~nxG
+for /f "tokens=1,2,3,4 delims=-." %%G in ("%pythonInstaller%") do set pythonVersion=%%H.%%I.%%J
+
+:: Run the installer
+echo Installing Python v%pythonVersion%...
+
+echo installing P
+pause
+exit /b
+
+msiexec /i /qb %pythonInstaller% /passive TargetDir=%pythonDir%
+if not [%ERRORLEVEL%]==[0] goto :PythonInstallFailed
+echo Python installed.
+
+
+	:ModuleInstallation
+
+echo installing modlules
+pause
+::exit /b
+
+:: Temporarily add python locations to PATH so we can reference pip. 
+:: The path variable for the current session may not have these yet.
+set PATH=%pythonDir%;%pythonDir%\Scripts;%PATH%
+
+echo.
+echo Installing supplementary Python modules...
+:: Install the python module wheels
+for %%G in ("*.whl") do (
+    set returnCode=
+    call :InstallModule "%%G" returnCode
+    if not !returnCode!==0 goto :ModuleInstallFailed
+)
+
+
+:: Success
+echo.
+echo Dependency installation complete.
+set exitCode=0
+goto Exit
+
+
+:PythonInstallFailed
+echo.
+echo Python installation failed.
+set exitCode=1
+goto Exit
+
+
+:ModuleInstallFailed
+echo.
+echo Supplementary module installation failed.
+set exitCode=2
+
+
+:Exit
+echo Press any key to exit . . .
+pause > nul
+exit /b %exitCode%
+
+
+:InstallModule
+:: Takes in 2 arguments: 
+::	- A relative path to a .whl file to install
+::	- A return code variable, so we can check the result of the installation
+for /f "tokens=1 delims=-" %%G in (%1) do set moduleName=%%G
+echo 	-^>  %moduleName%...
+%pythonDir%\python.exe -m pip install --no-index --disable-pip-version-check --no-python-version-warning %1 1> nul
+set %2=%ERRORLEVEL%
+goto :eof
