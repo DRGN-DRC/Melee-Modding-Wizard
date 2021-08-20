562a78a3bf5d5618cc1bbac7cddc68e30370a5a8
Durgan <72322229+DRGN-DRC@users.noreply.github.com>
2021-08-19 22:31:07 -0700

    Batch script tweaks

--- - - MMW_Debug.bat
+++ - - MMW_Debug.bat
@@ -1,8 +1,6 @@
 @echo off
 
-
 C:\Python27\python.exe "%~dp0main.py" %*
-
 
 :: Exit if no problems, otherwise pause so we can see what happened.
 if [%ERRORLEVEL%]==[0] goto eof
--- Asset Test.bat
+++ Asset Test.bat
@@ -3,8 +3,7 @@
 
 python "%~dp0main.py" test --path %*
 
-timeout 5
-
-if [%ERRORLEVEL%]==[0] exit /b
-
-pause
+:: Exit if no problems, otherwise pause so we can see what happened.
+if [%ERRORLEVEL%]==[0] goto eof
+pause > nul
+echo Press any key to exit . . .
