a5a7d4112f6150f18d9ee4cdd154b8f9a527a3a0
Durgan <72322229+DRGN-DRC@users.noreply.github.com>
2021-09-07 23:09:12 -0700

    Removed old batch scripts

--- Asset Test.bat
+++
@@ -1,9 +0,0 @@
-:: 
-@echo off
-
-python "%~dp0main.py" test --path %*
-
-:: Exit if no problems, otherwise pause so we can see what happened.
-if [%ERRORLEVEL%]==[0] goto eof
-pause > nul
-echo Press any key to exit . . .
